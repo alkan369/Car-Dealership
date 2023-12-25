4d46e55a496b9b1a636c8cac63e0934ba29a4927
Alkan Mustafa <alkan.mustafa@amusnet.com>
2023-12-25 19:02:00 +0200

    Initial stage

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+import { Application, json } from "express";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(json());
+
+app.get("/", (req, res) => {
+    return res.status(200).json({'message':'Hello World!'});
+});
+
+const PORT = process.env.PORT || 8080
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    });
+}
+catch(error){
+    console.log("Failed to start the server with error : ${error}");
+}
