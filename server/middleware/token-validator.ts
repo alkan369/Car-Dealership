61e71d7050fcbdfcbbe700337feef60d82d0a1e1
Alkan Mustafa <alkan.mustafa@amusnet.com>
2024-01-10 02:46:07 +0200

    Change token extraction logic

--- server/middleware/token-validator.ts
+++ server/middleware/token-validator.ts
@@ -4,19 +4,16 @@ import { verify } from "jsonwebtoken";
 export const validateToken = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     console.log("HEADER: ", authHeader);
-    const adminToken = authHeader.split(' ').pop().slice(1,6);
+    const adminToken = authHeader.split(' ').pop().slice(0,5);
 
-    const token = authHeader && (adminToken === 'Admin'? '"' + authHeader.split(' ').pop().slice(6) : authHeader.split(' ').pop());
+    const token = authHeader && (adminToken === 'Admin'? authHeader.split(' ').pop().slice(5) : authHeader.split(' ').pop());
 
     if (!token) {
         return res.status(401).send('Invalid token format');
     }
 
     try {
-        console.log("Alkan - adm token : ", adminToken);
-        console.log("Alkan - token : ", token);
         const user = verify(token, process.env.ACCESS_TOKEN_SECRET as string);
-        // console.log("User: ", username);
 
     } catch (err) {
         res.status(403).json({
